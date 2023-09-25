5a454a59bd1db053548c118c503f4026a3a9f42d
Wouter Laermans <wouter.laermans@appwise.be>
2023-09-25 10:42:58 +0200

    add http client

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,171 @@
-export const one = 1
-export const two = 2
+import type { Axios, AxiosRequestConfig } from 'axios'
+import { z } from 'zod'
+
+interface CustomizedError {
+  url: string
+  method: 'get' | 'post' | 'put' | 'delete'
+  error: z.ZodError
+}
+
+interface CreateHttpZodClientOptions {
+  axios: Axios
+  onZodError: (error: CustomizedError) => void
+}
+
+interface CreateHttpZodClientReturnType {
+  get: <T extends z.ZodType>(options: GetOptions<T>) => Promise<z.infer<T>>
+  post: <T extends z.ZodType>(options: PostOptions<T>) => Promise<z.infer<T>>
+  put: <T extends z.ZodType>(options: PutOptions<T>) => Promise<z.infer<T>>
+  delete: <T extends z.ZodType>(options: DeleteOptions<T>) => Promise<z.infer<T>>
+}
+
+interface GetOptions<T extends z.ZodType> {
+  url: string
+  responseSchema: T
+  config?: AxiosRequestConfig<any>
+}
+
+interface PostOptions<T extends z.ZodType> {
+  url: string
+  data: any
+  responseSchema: T
+  config?: AxiosRequestConfig<any>
+}
+
+interface PutOptions<T extends z.ZodType> {
+  url: string
+  data: any
+  responseSchema: T
+  config?: AxiosRequestConfig<any>
+}
+
+interface DeleteOptions<T extends z.ZodType> {
+  url: string
+  data?: any
+  responseSchema?: T
+  config?: AxiosRequestConfig<any>
+}
+
+export function createHttpZodClient(
+  { axios, onZodError }: CreateHttpZodClientOptions,
+): CreateHttpZodClientReturnType {
+  const get = async <T extends z.ZodType>(options: GetOptions<T>): Promise<z.infer<T>> => {
+    const { config, url, responseSchema } = options
+
+    const { data } = await axios.get(url, config)
+
+    try {
+      return responseSchema.parse(data)
+    }
+    catch (error) {
+      if (error instanceof z.ZodError) {
+        onZodError({
+          method: 'get',
+          url,
+          error,
+        })
+
+        return data
+      }
+
+      throw error
+    }
+  }
+
+  const post = async <T extends z.ZodType>(options: PostOptions<T>) => {
+    const {
+      url,
+      data,
+      config,
+      responseSchema,
+    } = options
+
+    const { data: responseData } = await axios.post(url, data, config)
+
+    try {
+      return responseSchema.parse(responseData)
+    }
+    catch (error) {
+      if (error instanceof z.ZodError) {
+        onZodError({
+          method: 'post',
+          url,
+          error,
+        })
+
+        return responseData
+      }
+
+      throw error
+    }
+  }
+
+  const put = async <T extends z.ZodType>(options: PutOptions<T>) => {
+    const {
+      url,
+      data,
+      config,
+      responseSchema,
+    } = options
+
+    const { data: responseData } = await axios.put(url, data, config)
+
+    try {
+      return responseSchema.parse(responseData)
+    }
+    catch (error) {
+      if (error instanceof z.ZodError) {
+        onZodError({
+          method: 'put',
+          url,
+          error,
+        })
+
+        return responseData
+      }
+
+      throw error
+    }
+  }
+
+  const del = async <T extends z.ZodType>(options: DeleteOptions<T>) => {
+    const {
+      url,
+      config,
+      data,
+      responseSchema,
+    } = options
+
+    const { data: responseData } = await axios.delete(url, {
+      ...(config ?? {}),
+      data,
+    })
+
+    if (!responseSchema)
+      return
+
+    try {
+      return responseSchema.parse(responseData)
+    }
+    catch (error) {
+      if (error instanceof z.ZodError) {
+        onZodError({
+          method: 'delete',
+          url,
+          error,
+        })
+
+        return responseData
+      }
+
+      throw error
+    }
+  }
+
+  return {
+    get,
+    post,
+    put,
+    delete: del,
+  }
+}
